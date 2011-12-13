443a56c714d14303e3060ca53b086f1ccfa83470
Yu Tu <tantofish@gmail.com>
2011-12-13 19:54:40 +0800

    i removed remote tantofish branch

--- src/ntu/csie/wcm/CanvasNetActivity.java
+++ src/ntu/csie/wcm/CanvasNetActivity.java
@@ -9,5 +9,7 @@ public class CanvasNetActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        //tantofish hahaha
     }
 }
