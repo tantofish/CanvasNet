0e82e653db9bc1471fbd10a9cffa52abc2eb54d1
Yu Tu <tantofish@gmail.com>
2011-12-15 01:48:51 +0800

    gallery about to be done

--- gen/ntu/csie/wcm/R.java
+++ gen/ntu/csie/wcm/R.java
@@ -13,16 +13,6 @@ public final class R {
     public static final class drawable {
         public static final int folder=0x7f020000;
         public static final int icon=0x7f020001;
-        public static final int img_1=0x7f020002;
-        public static final int img_2=0x7f020003;
-        public static final int img_3=0x7f020004;
-        public static final int img_4=0x7f020005;
-        public static final int photo_1=0x7f020006;
-        public static final int photo_2=0x7f020007;
-        public static final int photo_3=0x7f020008;
-        public static final int pic_1=0x7f020009;
-        public static final int pic_2=0x7f02000a;
-        public static final int pic_3=0x7f02000b;
     }
     public static final class id {
         public static final int ChangeColorBt=0x7f050002;
